16d839568281bf12a17728599843c63a5e0526b9
Vikram Modh <37219153+pantherofficial404@users.noreply.github.com>
2019-10-04 14:18:08 +0530

    Added Number of faces in code

--- opencvface.py
+++ opencvface.py
@@ -25,9 +25,9 @@ while(cam.isOpened()):
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces=face_detect.detectMultiScale(gray,1.3,5)
     eyes=eyes_detect.detectMultiScale(gray,1.3,5)
+    print("Number of faces",len(faces))
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-        
         for (x,y,w,h) in eyes:
             cv2.rectangle(img,(x,y),(x+h,y+h),(0,0,255),2)
     cv2.imshow("faces",img)
